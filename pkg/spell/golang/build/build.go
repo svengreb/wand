060b3328c2c5132b770fb10c0f9b2ebcfaec1fb8
Sven Greb <development@svengreb.de>
2020-11-23 21:10:06 +0100

    Spell incantation for Go toolchain "build" command (#28)
    To run the `go build` command of the Go toolchain, the new
    `spell.Incantation` [1] has been implemented in the new build [2]
    package that can be used through a Go toolchain caster [3].
    The spell incantation can be configured through the following functions:
    
    - `WithBinaryArtifactName(name string) build.Option` - sets the name for
      the binary build artifact.
    - `WithCrossCompileTargetPlatforms(platforms ...string) build.Option` -
      sets the names of cross-compile platform targets.
    - `WithFlags(flags ...string) build.Option` - sets additional flags to
      pass to the Go `build` command along with the base Go flags.
    - `WithGoOptions(goOpts ...spellGo.Option) build.Option` - sets shared
      Go toolchain commands options.
    - `WithOutputDir(dir string) build.Option` - sets the output directory,
      relative to the project root, for compilation artifacts.
    
    To unify further implementations for the Go toolchain, a new `Options`
    struct type has also been implemented in the golang [4] package that
    stores global/shared Go toolchain options that are shared between
    multiple Go toolchain commands:
    
    - `WithAsmFlags(asmFlags ...string) golang.Option` - sets flags to pass
      on each `go tool asm` invocation.
    - `WithRaceDetector(enableRaceDetector bool) golang.Option` - indicates
      if the race detector should be enabled.
    - `WithTrimmedPath(enableTrimPath bool) golang.Option` - indicates if
      all file system paths should be removed from the resulting executable.
    - `WithEnv(env map[string]string) golang.Option` - adds or overrides Go
      toolchain command specific environment variables.
    - `WithFlags(flags ...string) golang.Option` - sets additional Go
      toolchain command flags.
    - `WithFlagsPrefixAll(flagsPrefixAll bool) golang.Option` - indicates if
      the values of `-asmflags` and `-gcflags` should be prefixed with the
        `all=` pattern in order to apply to all packages.
    - `WithGcFlags(gcFlags ...string) golang.Option` - sets flags to pass on
      each `go tool compile` invocation.
    - `WithLdFlags(ldFlags ...string) golang.Option` - sets flags to pass on
      each `go tool link` invocation.
    - `WithMixins(mixins ...spell.Mixin) golang.Option` - sets
      `spell.Mixin`s that can be applied by option consumers.
    - `WithTags(tags ...string) golang.Option` - sets Go toolchain tags.
    
    The new `CompileFormula(opts ...Option) []string` function [5] can be
    used to compile the formula for these options.
    
    [1]: https://pkg.go.dev/github.com/svengreb/wand/pkg/spell#Incantation
    [2]: https://pkg.go.dev/github.com/svengreb/wand/pkg/spell/golang/build
    [3]: https://pkg.go.dev/github.com/svengreb/wand/pkg/cast/golang#Caster
    [4]: https://pkg.go.dev/github.com/svengreb/wand/pkg/spell/golang
    [5]: https://pkg.go.dev/github.com/svengreb/wand/pkg/spell/golang#CompileFormula
    
    Closes GH-27

---
+++ pkg/spell/golang/build/build.go
@@ -0,0 +1,77 @@
+// Copyright (c) 2019-present Sven Greb <development@svengreb.de>
+// This source code is licensed under the MIT license found in the LICENSE file.
+
+// Package build provide a spell incantation for the "build" command of the Go toolchain.
+package build
+
+import (
+	"path/filepath"
+
+	"github.com/svengreb/wand/pkg/app"
+
+	"github.com/svengreb/wand"
+	"github.com/svengreb/wand/pkg/spell"
+	spellGo "github.com/svengreb/wand/pkg/spell/golang"
+)
+
+// Spell is a spell incantation for the "build" command of the Go toolchain.
+type Spell struct {
+	ac   app.Config
+	opts *Options
+}
+
+// Formula returns the spell incantation formula for the "build" command of the Go toolchain.
+// Note that configured flags are applied after the "GOFLAGS" environment variable and could overwrite already defined
+// flags.
+// See `go help environment`, `go help env` and the `go` command documentations for more details:
+//   - https://golang.org/cmd/go/#hdr-Environment_variables
+func (s *Spell) Formula() []string {
+	args := []string{"build"}
+
+	args = append(args, spellGo.CompileFormula(s.opts.spellGoOpts...)...)
+
+	if len(s.opts.Flags) > 0 {
+		args = append(args, s.opts.Flags...)
+	}
+
+	args = append(
+		args,
+		"-o",
+		filepath.Join(s.opts.OutputDir, s.opts.BinaryArtifactName),
+		s.ac.PkgPath,
+	)
+
+	return args
+}
+
+// Kind returns the spell incantation kind.
+func (s *Spell) Kind() spell.Kind {
+	return spell.KindBinary
+}
+
+// Options returns the spell incantation options.
+func (s *Spell) Options() interface{} {
+	return *s.opts
+}
+
+// Env returns spell incantation specific environment variables.
+func (s *Spell) Env() map[string]string {
+	return s.opts.Env
+}
+
+// New creates a new spell incantation for the "build" command of the Go toolchain.
+//nolint:gocritic // The app.Config struct is passed as value by design to ensure immutability.
+func New(wand wand.Wand, ac app.Config, opts ...Option) *Spell {
+	opt := newOptions(opts...)
+
+	if opt.BinaryArtifactName == "" {
+		opt.BinaryArtifactName = ac.Name
+	}
+
+	// Store build artifacts in the application specific subdirectory.
+	if opt.OutputDir == "" {
+		opt.OutputDir = ac.BaseOutputDir
+	}
+
+	return &Spell{ac: ac, opts: opt}
+}
