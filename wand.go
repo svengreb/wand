a4e2a38f84af735be77297182f864f82e846eece
Sven Greb <development@svengreb.de>
2021-11-23 22:30:51 +0100

    Update to `tmpl-go` template repository version `0.11.0` (#113)
    Updated to `tmpl-go` version `0.11.0` [1] which...
    
    1. fixed `golangci-lint` running errors due to `revive`s unknown
       `time-equal` rule [2].
    2. disabled the `revive` linter rule `package-comments` [3].
    
    See the full `tmpl-go` version `0.10.1` changelog [1] for all details.
    
    [1]: https://github.com/svengreb/tmpl-go/releases/tag/v0.10.1
    [2]: https://github.com/svengreb/tmpl-go/issues/76
    [3]: https://github.com/svengreb/tmpl-go/issues/78
    
    Closes GH-112

--- wand.go
+++ wand.go
@@ -1,6 +1,7 @@
 // Copyright (c) 2019-present Sven Greb <development@svengreb.de>
 // This source code is licensed under the MIT license found in the LICENSE file.
 
+// Package wand manages a project and its applications and stores their metadata.
 package wand
 
 import (
